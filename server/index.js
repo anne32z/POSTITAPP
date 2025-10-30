201b0b81d490068b291bd8b8ba9e52f1756beaa8
anne32z <anne3323z@gmail.com>
2025-10-30 09:47:16 +0400

    server-registerUser

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import cors from "cors";
 import express from "express";
-
-
+import UserModel from "./Models/UserModel.js";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(express.json());
@@ -11,7 +11,7 @@ app.use(cors());
  
 
 //Database connection
-const connectString ="";
+const connectString ="mongodb+srv://admin:12345@postitcluster.fkp8kq5.mongodb.net/postITDb?appName=PostITCluster";
 
  
 
@@ -20,7 +20,26 @@ mongoose.connect(connectString, {
   useUnifiedTopology: true,
 
 });
+// POST API for register
+app.post("/registerUser",async(req,res)=>{
+    try{
+        const name=req.body.name;
+        const email=req.body.email;
+        const password=req.body.password;
+        const hashedpassword=await bcrypt.hash(password,10);
 
+        const user=new UserModel({
+            name:name,
+            email:email,
+            password:hashedpassword,
+        });
+        await user.save();
+        res.send({user:user,msg:"Added."});
+    }catch{
+        
+        res.status(500).json({ error: "An error occurred" });
+    }
+});
 
 app.listen(3001, () => {
   console.log("You are connected");
