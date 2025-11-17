2188110024688c38365d840433a3d1757cf30ae7
anne32z <anne3323z@gmail.com>
2025-11-17 13:43:44 +0400

    Act13 is done

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -1,9 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from "../Features/UserSlice"; //import the reducer
+import usersReducer from "../Features/UserSlice";
 import postReducer from "../Features/PostSlice";
-export const store = configureStore({
-  reducer: {
-    users: usersReducer,
-    posts: postReducer,
-  },
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+
+// Redux Persist config
+const persistConfig = {
+  key: "reduxstore",
+  storage,
+};
+
+// Combine reducers
+const rootReducer = combineReducers({
+  users: usersReducer,
+  posts: postReducer,
 });
+
+// Create persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// 1️⃣ Create the store first
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// 2️⃣ Then create the persistor
+export const persistor = persistStore(store);
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { store } from "./Store/store";
 import { Provider } from "react-redux";
 import Register from "./Components/Register";
-
+import { store,persistor } from "./Store/store";
+import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+        {/* Wrapping with PersistGate */}
+
+      <PersistGate loading={null} persistor={persistor}>
+
+        <App />
+
+      </PersistGate>
       {/* <Register /> */}
     </React.StrictMode>
   </Provider>
