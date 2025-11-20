6f329925e81741f7bb93444fc2c33327709406c4
anne32z <anne3323z@gmail.com>
2025-11-20 08:38:00 +0400

    changes the env place outside the src

--- client/src/Features/UserSlice.js
+++ client/src/Features/UserSlice.js
@@ -57,23 +57,7 @@ export const logout = createAsyncThunk("/users/logout", async () => {
 export const userSlice = createSlice({
   name: "users", //name of the state
   initialState, // initial value of the state
-  // reducers: {
-  //   addUser: (state, action) => {
-  //     state.value.push(action.payload);
-  //   },
-  //   deleteUser: (state, action) => {
-  //     state.value = state.value.filter((user) => user.email !== action.payload);
-  //   },
-  //   updateUser: (state, action) => {
-  //     state.value.map((user) => {
-  //       //iterate the  array and compare the email with the email from the payload
-  //       if (user.email === action.payload.email) {
-  //         user.name = action.payload.name;
-  //         user.password = action.payload.password;
-  //       }
-  //     });
-  //   },
-  // },
+ 
   extraReducers: (builder) => {
     builder
       .addCase(registerUser.pending, (state) => {
